3d0bbc3e96c616b0f3ef0df385e7c6dfd8818e6b
stavbers <reddevweb@gmail.com>
2022-09-22 00:42:32 +0300

    add screen5 html css js img

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,26 @@
 import './_functions';
+import Accordion from 'accordion-js';
 
+let accordion = new Accordion('.accordion-container', {
+  // animation duration in ms {number} 
+  duration: 600, 
+  // add ARIA elements to the HTML structure {boolean}
+  ariaEnabled: true, 
+  // allow collapse expanded panel {boolean}
+  collapse: true, 
+  // show multiple elements at the same time {boolean}
+  showMultiple: false, 
+  // disabling this option will find all items in the container {boolean}
+  onlyChildNodes: true, 
+  // show accordion elements during initialization {array}
+  openOnInit: [], 
+  // element class {string}
+  elementClass: 'ac', 
+  // trigger class {string}
+  triggerClass: 'ac-trigger', 
+  // panel class {string}
+  panelClass: 'ac-panel', 
+  // active element class {string}
+  activeClass: 'is-active'
+  
+});
